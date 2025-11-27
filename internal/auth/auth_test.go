98e44e7e755a4f9cf0a328a497c0c33e839c0a89
Matej Alexander <matejalexander@gmail.com>
2025-11-27 19:36:36 +0100

    added unit test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+
+	cases := []struct {
+		input    http.Header
+		expected string
+	}{
+		{
+			input:    http.Header{"Authorization": []string{"ApiKey test1"}},
+			expected: "test1",
+		},
+		{
+			input: http.Header{},
+		},
+		{
+			input:    http.Header{"User-Agent": []string{"matejalex"}},
+			expected: "",
+		},
+		{
+			input:    http.Header{"User-Agent": []string{"matejalex"}, "Authorization": []string{"ApiKey test1"}},
+			expected: "test2",
+		},
+	}
+
+	for _, c := range cases {
+		actual, _ := GetAPIKey(c.input)
+		if actual != c.expected {
+			t.Errorf("Results do not match, input: %+v [returned:expected] %s:%s", c.input, actual, c.expected)
+		}
+	}
+
+}
